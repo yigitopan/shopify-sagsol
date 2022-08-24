4023e01f7affa0e8118b586fc2b2cd5cf3997a51
yigitopan <yigitopan@hotmail.com>
2022-08-24 11:41:03 +0300

    first commit

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -1,28 +1,30 @@
-
-const request = require('request');
+const axios = require('axios');
 
 const getItems = async(req, res, next) => {
-    let body;
+    let data;
     try {
-        const options = {
-            'method': 'GET',
-            'url': 'http://ws.neko.com.tr/api/categories',
-            'headers': {
+        const config = {
+            method: 'get',
+            url: 'http://ws.neko.com.tr/api/categories',
+            headers: {
                 'Content-Type': 'application/json',
                 'Authorization': 'bearer 3MvwzLpm2W_aJR5XIs5tkNHqFhEUagbyLJN51EhwsI_y9kZX-aqUJlirJl5Xmgo72A_EaeeLyhYgqmHCAeuq1wrqBTUAjjdUMxmOBT7RUYaaQ0UNZNTbo1ugwXy4z7T9ULeuioJRYlLjA9SzS0IAY0jBYIPFYg9yAm3PTMtVUenanL5uiognCWi68XjqEns10qlZ0c_zQ4b-78-v89AhSC66ebACRl2r9Y0E9vOC7rfjYqwQe4n0fOrjr_K2Pmf1'
             }
         };
-        request(options, function (error, response) {
-            body = response.body
-            if (error) throw new Error(error);
-            console.log(response.body);
-        });
 
+        axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                data = JSON.stringify(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
     catch (err){
         return next(err);
     }
-    res.status(200).json({message:body});
+    res.status(200).json({message:data});
 }
 
 module.exports = {

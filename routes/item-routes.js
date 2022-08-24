297061c6ed1a4a3ddbe1a28e681ddceaed49d2b0
yigitopan <yigitopan@hotmail.com>
2022-08-24 11:32:37 +0300

    first commit

--- routes/item-routes.js
+++ routes/item-routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const itemController = require('../controllers/item-controller');
 const router = express.Router();
 
-router.post('/delete', itemController.getItems);
+router.get('/get', itemController.getItems);
 
 module.exports = router;
